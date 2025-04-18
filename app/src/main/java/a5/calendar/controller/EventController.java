23d65edaa606896cb9cf80db07ffa4a9b9552ce0
Jose Granados Ruiz <151814924+RM64@users.noreply.github.com>
2025-04-18 14:39:52 -0500

    Update EventController.java

--- app/src/main/java/a5/calendar/controller/EventController.java
+++ app/src/main/java/a5/calendar/controller/EventController.java
@@ -1,4 +1,47 @@
 package a5.calendar.controller;
 
+import java.util.Comparator;
+import a5.calendar.model.Event;
+
 public class EventController {
+    public static final Comparator<Event> BY_DATE_CATEGORY_NAME =
+            (a, b) -> {
+
+                int comp = Integer.compare(a.getMonth(), b.getMonth());
+                if (comp != 0) {
+                    return comp;
+                }
+
+                comp = Integer.compare(a.getDay(), b.getDay());
+                if (comp != 0) {
+                    return comp;
+                }
+
+                comp = a.getCategory().compareTo(b.getCategory());
+                if (comp != 0) {
+                    return comp;
+                }
+
+                return a.getName().compareTo(b.getName());
+            };
+
+    public static final Comparator<Event> BY_CATEGORY_DATE_NAME =
+            (a, b) -> {
+                int comp = a.getCategory().compareTo(b.getCategory());
+                if (comp != 0) {
+                    return comp;
+                }
+
+                return BY_DATE_CATEGORY_NAME.compare(a, b);
+            };
+
+    public static final Comparator<Event> BY_NAME_DATE_CATEGORY =
+            (a, b) -> {
+                int comp = a.getName().compareTo(b.getName());
+                if (comp != 0) {
+                    return comp;
+                }
+
+                return BY_DATE_CATEGORY_NAME.compare(a, b);
+            };
 }
