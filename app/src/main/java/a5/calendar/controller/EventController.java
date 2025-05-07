10e4fbb3e9a331ef2c6405ceabfa3cc236dcdc80
Javier <62152194+Vettel53@users.noreply.github.com>
2025-05-06 22:16:35 -0500

    Javadoc EventController.java

--- app/src/main/java/a5/calendar/controller/EventController.java
+++ app/src/main/java/a5/calendar/controller/EventController.java
@@ -3,7 +3,14 @@ package a5.calendar.controller;
 import java.util.Comparator;
 import a5.calendar.model.Event;
 
+/**
+ * Controller class for handling event sorting based on different criteria.
+ */
 public class EventController {
+
+     /**
+     * Comparator for sorting events by month, then day, then category, and lastly by name.
+     */
     public static final Comparator<Event> BY_DATE_CATEGORY_NAME =
             (a, b) -> {
 
@@ -25,6 +32,9 @@ public class EventController {
                 return a.getName().compareTo(b.getName());
             };
 
+    /**
+     * Comparator for sorting events by category, then by date, and lastly by name.
+     */
     public static final Comparator<Event> BY_CATEGORY_DATE_NAME =
             (a, b) -> {
                 int comp = a.getCategory().compareTo(b.getCategory());
@@ -35,6 +45,9 @@ public class EventController {
                 return BY_DATE_CATEGORY_NAME.compare(a, b);
             };
 
+    /**
+     * Comparator for sorting events by name, then by date, and finally by category.
+     */
     public static final Comparator<Event> BY_NAME_DATE_CATEGORY =
             (a, b) -> {
                 int comp = a.getName().compareTo(b.getName());
